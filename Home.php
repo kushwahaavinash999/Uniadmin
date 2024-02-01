9587202110d3fe8c21bf769502c57b49a4fc31d5
Abhinash <123896446+kushwahaavinash999@users.noreply.github.com>
2024-02-01 23:27:18 +0530

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="Home.css" />
+
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Collage Demo</title>
+</head>
+
+<body>
+    <header>
+        <div class="Logo-Title">
+            <img src="logo.jpg" id="logo" />
+            <h1 class="collage_name">ABC ENGINEERING COLLEGE</h1>
+        </div>
+        <nav class="navbar navbar-expand-lg bg-black">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Programs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Admission</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Departments</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Placements</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="database/signup.php">Signup</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="database/signindashboard.php">Signin Dashboard</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-primary" type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="latest-news">
+        <marquee>Dear Student Admission process has been started for academic year
+            {Academic Year 2023-23}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Confirm your admission before it too late.
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            ABC School organised sports meat on 6 to 8 march
+            2023.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ABC
+            School organised science exhibition.</marquee>
+    </div>
+    <main>
+        <div class="slideshow-container">
+            <div class="mySlides fade">
+                <img src="banner/1st.jpg.jpg" />
+            </div>
+
+            <div class="mySlides fade">
+                <img src="banner/2nd.jpg.jpg" />
+            </div>
+
+            <div class="mySlides fade">
+                <img src="banner/3rd.jpg.jpg" />
+            </div>
+        </div>
+        <br />
+
+        <div style="text-align: center">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+    </main>
+
+    <div class="body_box">
+        <div class="card" style="width: 18rem">
+            <img src="Card-img/af2.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">Academic Facilities</h5>
+                <p class="card-text">
+                    ABC Engineering College provides a magniﬁcent and comprehensive
+                    range of facilities with in the campus for its students. The
+                    classrooms at ABC are contemporary, equipped with ceiling mounted
+                    LCD projectors. The college frequently organizes National &
+                    International level Conferences and Seminars on various themes and
+                    disciplines to promote industry interaction for its students. The
+                    auditorium at ABC is up to date with state-of-the-art multimedia
+                    systems which can be used for presentations, seminars,fests and
+                    other academic events. The library is well equipped with avast
+                    number of books, journals, magazines and newspapers for students.
+                    The development of state-of-the-art infrastructure and technical
+                    facilities has been a continuous process at ABC Engineering College.
+                    The laboratories provide practical exposure to various subjects and
+                    give an opportunity to the students to experiment with what they
+                    have learnt in classrooms.
+                </p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+            <img src="Card-img/relay2.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">Sports Facilities</h5>
+                <p class="card-text">
+                    A healthy mind resides in a healthy body. Physical activities help
+                    the students to keep them physically fit and active. Our sports
+                    centers provide opportunities and encourage the students to
+                    participate in intra-college, inter-college and university level
+                    sports competitions. ABC provides state of the art facilities with
+                    outdoor play grounds, Floodlit stadium, Swimming pool, Lawn Tennis,
+                    Badminton, Volleyball and Basketball Courts, Gymnasium and several
+                    sporting activities that help students to stay fit and to develop a
+                    spirit of sportsmanship.
+                </p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+            <img src="Card-img/COMPUTER-1024x614.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">Campus Facilities</h5>
+                <p class="card-text">
+                    ABC campus is well equipped with a wide variety of modern and
+                    upgraded facilities to create a conducive environment where budding
+                    professionals can shape their future. The college campus is well
+                    developed with latest facilities and modern amenities. The pleasant,
+                    friendly and harmonious campus life offers opportunities for the
+                    students to pursue their career, as well as boost the involvement of
+                    the students for participating in various extra-curricular
+                    activities. The campus also caters refreshment to the students
+                    through strategically located cafes, juice corners, canteens and
+                    utility centers. We give massive importance to our facilities and
+                    learning resources and continuously strive to ensure, that our
+                    students have full access to the enriched resources, giving them
+                    ample opportunities to groom into well nurtured professionals with
+                    industry ready skills. From time to time cultural and technical
+                    festivals, project competitions, motivational lectures, guest
+                    lectures and philanthropic activities are held for the overall
+                    development of the students. The college provides a platform for the
+                    students with ample opportunities by offering value added trainings,
+                    workshops and campus interviews, so that the students are able to
+                    contribute towards the betterment of the Industry and the society at
+                    large. The college campus gives utmost importance to safety
+                    measures; fire- fighting equipments and CCTV cameras are fixed at
+                    strategic locations all over the campus.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="body_box">
+        <div class="card" style="width: 18rem">
+            <img src="Card-img/af2.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">Residential Facilities</h5>
+                <p class="card-text">
+                    ABC provides accommodation to the students on a
+                    first-come-first-served basis. The campus is fully residential and
+                    extends a warm and cordial environment full of opportunities for
+                    personal growth and holistic development of the students. The hostel
+                    provides a feeling of "a Home-Away-From-Home" to the students.
+                    Keeping in view the comfort and ideal hospitality, we provide a
+                    secure, comfortable, hygienic and healthy environment for the
+                    hostellers. The hostel offers excellent lodging, boarding and
+                    laundry facilities to cater to the needs of boys and girls. We have
+                    three girls’ hostels and five boys’ hostels in the campus which
+                    accommodate around 900 students. Each hostel is supervised by a
+                    warden and a faculty-in-charge, who ensures that utmost care, is
+                    taken to provide the best amenities to the hostellers. To ensure
+                    security and safety of the hostel inmates, 24X7 security guards are
+                    deputed on duty and the hostels are under CCTV surveillance. Each
+                    hostel complex has a dedicated mess with well equipped kitchen and
+                    dining space providing hygienic, nutritious and wholesome meals. A
+                    wide range of cultural, sports and social activities are conducted
+                    for the merriment of the students. In addition to the various
+                    facilities, hostels have exclusive tennis court, basketball court,
+                    badminton court and a modernized gymnasium for facilitating health
+                    and fitness.
+                </p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+            <img src="Card-img/1.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">Library</h5>
+                <p class="card-text">
+                    Synonymous with the abode of knowledge, library of ABC is stocked
+                    with around 1.14 lac books, research journals, reports, multi-volume
+                    reference works, dictionaries, encyclopedias and handbooks to propel
+                    the academic and research growth of the students and faculty
+                    members. The library is periodically re-equipped with all the
+                    printed and e-resources from diverse streams, to provide for the
+                    most recent tools, to support teaching, learning and research
+                    activities. The library also facilitates access to a number of
+                    e-journals and ebooks through its participation in Nalanda
+                    E-consortia(AKTU). The library is instrumental to support
+                    information literacy and profound learning skills.
+                </p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem">
+            <img src="Card-img/a4.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">Auditorium and Seminar Hall</h5>
+                <p class="card-text">
+                    <strong>Auditorium:</strong> A well- furnished fully air-
+                    conditioned auditorium (Raman Auditorium) with state- of- the- art
+                    audio, video facilities, access to the internet with approximate 500
+                    seating capacity. The auditorium facilitates for conduct of
+                    international, national and other college events such as
+                    conferences, seminars, placement programs, convocation, cultural
+                    events and theatrical plays. The auditorium features, fixed
+                    traditional theatre style seating, raised stage, adjustable
+                    lighting, acoustics, large screen for video presentations, and
+                    vaulted ceiling. It is designed to ensure a perfect view of the main
+                    stage, irrespective of the seating location of the audience.
+
+                    <strong>Seminar Hall:</strong>strong> ABC has well- furnished and
+                    spacious, fully air- conditioned Seminar Halls with audio- video
+                    facilities for smooth conduct of lectures by Guest Speakers,
+                    Exhibitions, Seminars, MDP programmes and conferences.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="foot_box"></div>
+    </footer>
+</body>
+<script src="banner.js"></script>
+
+</html>
